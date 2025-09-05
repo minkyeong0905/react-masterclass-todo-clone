6352f580d06d278afce00a573b68e66e8ba02877
minkyeong0905 <mingit0905@naver.com>
2025-09-05 16:16:15 +0900

    Refactoring

--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -1,6 +1,7 @@
-//import { useState } from "react";
-import { useForm } from "react-hook-form";
-import { atom, useRecoilState, useRecoilValue, useSetRecoilState } from "recoil";
+import { useRecoilValue } from "recoil";
+import CreateToDo from "./CreateToDo";
+import { toDoState } from "../atoms";
+import ToDo from "./ToDo";
 
 // function ToDoList() {
 //     const [toDo, setToDo] = useState("");
@@ -93,44 +94,17 @@ import { atom, useRecoilState, useRecoilValue, useSetRecoilState } from "recoil"
 //     );
 // }
 
-interface IForm {
-    toDo: string;
-}
-
-interface IToDo {
-    text: string;
-    id: number;
-    category: "TO_DO" | "DOING" | "DONE";
-}
-
-const toDoState = atom<IToDo[]>({
-    key: "toDo"
-   ,default: []
-});
-
 function ToDoList() {
-    const [toDos, setToDos] = useRecoilState(toDoState);
-    // const value = useRecoilValue(toDoState);
-    // const modFn = useSetRecoilState(toDoState);
-    const { register, handleSubmit, setValue } = useForm<IForm>();
-    //const handleValid = (data: IForm) => {
-    const handleValid = ({ toDo }: IForm) => {
-        setToDos((oldToDos) => [{text: toDo, id: Date.now(), category: "TO_DO"}, ...oldToDos]);
-        setValue("toDo", "");
-    };
+    const toDos = useRecoilValue(toDoState);
     
     return (
         <div>
             <h1>To Dos</h1>
             <hr />
-            <form onSubmit={handleSubmit(handleValid)}>
-                <input {...register("toDo", {
-                    required: "Please write a To Do."
-                })} placeholder="Write a to do." />
-                <button>Add</button>
-            </form>
+            <CreateToDo />
             <ul>
-                {toDos.map((toDo) => <li key={toDo.id}>{toDo.text}</li>)}
+                {/* {toDos.map((toDo) => <ToDo text={toDo.text} id={toDo.id} category={toDo.category} />)} */}
+                {toDos.map((toDo) => <ToDo key={toDo.id} {...toDo} />)}
             </ul>
         </div>
     );
