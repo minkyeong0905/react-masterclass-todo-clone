02c2e3c6bc92b42f929028b6cf51b89ff7cb2dee
minkyeong0905 <mingit0905@naver.com>
2025-09-09 17:24:08 +0900

    Updated atoms.tsx (Using enum)

--- src/atoms.tsx
+++ src/atoms.tsx
@@ -1,16 +1,21 @@
 import { atom, selector } from "recoil";
 
-type categories = "TO_DO" | "DOING" | "DONE";
+//type categories = "TO_DO" | "DOING" | "DONE";
+export enum Categories {
+    "TO_DO" = "TO_DO"
+   ,"DOING" = "DOING"
+   ,"DONE" = "DONE"
+};
 
 export interface IToDo {
     text: string;
     id: number;
-    category: categories;
+    category: Categories;
 }
 
-export const categoryState = atom<categories>({
+export const categoryState = atom<Categories>({
     key: "catogory"
-   ,default: "TO_DO"
+   ,default: Categories.TO_DO
 });
 
 export const toDoState = atom<IToDo[]>({
--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -1,5 +1,5 @@
 import { useSetRecoilState } from "recoil";
-import { IToDo, toDoState } from "../atoms";
+import { Categories, IToDo, toDoState } from "../atoms";
 
 function ToDo({ text, category, id }: IToDo) {
     // const onClick = (newCategory: IToDo["category"]) => {
@@ -25,9 +25,9 @@ function ToDo({ text, category, id }: IToDo) {
             {category !== "DOING" && <button onClick={() => onClick("DOING")}>Doing</button>}
             {category !== "DONE" && <button onClick={() => onClick("DONE")}>Done</button>} */}
             
-            {category !== "TO_DO" && <button name="TO_DO" onClick={onClick}>To Do</button>}
-            {category !== "DOING" && <button name="DOING" onClick={onClick}>Doing</button>}
-            {category !== "DONE" && <button name="DONE" onClick={onClick}>Done</button>}
+            {category !== Categories.TO_DO && <button name={Categories.TO_DO} onClick={onClick}>To Do</button>}
+            {category !== Categories.DOING && <button name={Categories.DOING} onClick={onClick}>Doing</button>}
+            {category !== Categories.DONE && <button name={Categories.DONE} onClick={onClick}>Done</button>}
         </li>
     );
 }
